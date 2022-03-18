640869d4cc84d44e78df76a7106edaf8d6c21fe4
Prabir Tarafdar <ptarafdar010@gmail.com>
2022-03-18 09:11:06 +0530

    update sattings.py

--- bankWebApp/settings.py
+++ bankWebApp/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bank',
-    'whitenoise.runserver_nostatic'
+    # 'whitenoise.runserver_nostatic'
 ]
 
 MIDDLEWARE = [
